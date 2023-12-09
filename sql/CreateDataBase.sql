001197c87c8dc3128100a1d9b98696fde9ed527e
Ottirate <sarah.hautot76@gmail.com>
2023-12-09 19:14:43 +0100

    Modification base de donnée : ajout des méthodes.

--- sql/CreateDataBase.sql
+++ sql/CreateDataBase.sql
@@ -2,8 +2,11 @@
 
 
 DROP TABLE IF EXISTS Hunts;
+DROP TABLE IF EXISTS HuntsMethods;
+
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Pokemons;
+DROP TABLE IF EXISTS Games;
 
 
 
@@ -12,7 +15,8 @@ CREATE TABLE Users
 	iduser    INTEGER     AUTO_INCREMENT PRIMARY KEY,
 	loginuser VARCHAR(50) NOT NULL   UNIQUE,
 	mailuser  VARCHAR(50) NOT NULL   UNIQUE,
-	pwduser   VARCHAR(50) NOT NULL
+	pwduser   VARCHAR(50) NOT NULL         ,
+	nivAcces  INTEGER     DEFAULT 0
 );
 
 INSERT INTO Users (iduser, loginuser, mailuser, pwduser) VALUES (1, 'Ottirate', 'ezezezez', 'shiny');
@@ -23,10 +27,30 @@ CREATE TABLE Pokemons
 (
 	numpokedex  INTEGER      PRIMARY KEY,
 	nompokemon  VARCHAR(30)  NOT NULL   ,
-	generation  INTEGER      NOT NULL   ,
+	generation  INTEGER      NOT NULL   CHECK generation > 0,
 	imgnonshiny VARCHAR(255) NOT NULL   ,
 	imgshiny    VARCHAR(255) NOT NULL   
 );
+
+CREATE TABLE JeuxPokemon 
+(
+    nom        VARCHAR(60) PRIMARY KEY,
+    generation INT  NOT NULL CHECK generation > 0,
+    date       DATE NOT NULL
+);
+
+
+
+CREATE TABLE HuntsMethods
+(
+	nom    VARCHAR (30) NOT NULL,                          --Nom de la méthode
+	jeux   VARCHAR (60) REFERENCES JeuxPokemon(jeux),      --Nom du jeu
+	oddsSC FLOAT        CHECK BETWEEN 0 AND 1,             --Odds avec shiny charm
+	odds   FLOAT        CHECK BETWEEN 0 AND 1,             --Odds sans
+	PRIMARY KEY (nom, jeux)
+);
+
+
 
 
 CREATE TABLE Hunts 
@@ -35,5 +59,7 @@ CREATE TABLE Hunts
 	idpokemon   INTEGER NOT NULL    REFERENCES Pokemons(numpokedex),
 	nbrencontre INTEGER DEFAULT 0                                  ,
 	estfinit    BOOLEAN DEFAULT FALSE                              ,
+	imgshiny    BOOLEAN DEFAULT TRUE                              ,
 	iduser      INTEGER REFERENCES Users(iduser)
 );
+
